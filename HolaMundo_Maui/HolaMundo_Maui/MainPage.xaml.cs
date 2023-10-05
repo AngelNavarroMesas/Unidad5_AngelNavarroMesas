a54d9f490ca5835e8838e745c5e346bf5de72bf4
AngelNavarroMesas <angel.navarro@iesnervion.es>
2023-10-05 14:34:45 +0200

    Agregar archivos de proyecto.

---
+++ HolaMundo_Maui/HolaMundo_Maui/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using biblioteca;
+
+namespace HolaMundo_Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        private ClsPersona persona = new ClsPersona();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+
+            string apellido = await DisplayPromptAsync("Pregunta", "Cual es tu apellido");
+
+            if (!string.IsNullOrEmpty(nombre.Text) && !string.IsNullOrEmpty(apellido))
+            {
+                await DisplayAlert("Hola",nombre.Text+" "+apellido, "OK");
+            }
+        }
+    }
+}
