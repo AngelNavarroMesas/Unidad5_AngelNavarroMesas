d3bf214fa22a789e00d08bdbbedb43e69cc0963d
AngelNavarroMesas <angel.navarro@iesnervion.es>
2023-10-10 14:40:55 +0200

    comiti

---
+++ Ejercicio1_Unidad6/Ejercicio1_Unidad6/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+
+namespace Ejercicio1_Unidad6
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Click(object sender, EventArgs e)
+        {
+            Button  boton3 = new Button {
+                Text = "boton1",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Background = Color.FromArgb("0008ff"),
+                HeightRequest = 70,
+                WidthRequest = 200,
+                FontFamily = "Verdana",
+                FontAttributes = FontAttributes.Bold,
+                FontSize = 16,
+                BorderColor = Color.FromArgb("#f0fc03"),
+                Margin = 30
+            };
+        }
+    }
+}
---
+++ Ejercicio1_Unidad6/Ejercicio1_Unidad6/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Ejercicio1_Unidad6
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
