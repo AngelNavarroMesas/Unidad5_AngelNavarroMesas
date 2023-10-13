ab94d0b44e4206635f6205c0b0ef88378b97646b
AngelNavarroMesas <angel.navarro@iesnervion.es>
2023-10-13 11:03:58 +0200

    comiti

--- Ejercicio1_Unidad6/Ejercicio1_Unidad6/MainPage.xaml.cs
+++ Ejercicio1_Unidad6/Ejercicio1_Unidad6/MainPage.xaml.cs
@@ -6,28 +6,44 @@ namespace Ejercicio1_Unidad6
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void Click(object sender, EventArgs e)
+        Boolean btnYaCreado = false;
+
+        private void Click(object sender, EventArgs e){
+
+            Button boton3 = new Button();
+
+            if (btnYaCreado==false) {
+                boton3 = new Button {
+                    Text = "boton3",
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.Center,
+                    Background = Color.FromArgb("0008ff"),
+                    HeightRequest = 70,
+                    WidthRequest = 200,
+                    FontFamily = "Verdana",
+                    FontAttributes = FontAttributes.Bold,
+                    FontSize = 16,
+                    BorderColor = Color.FromArgb("#f0fc03"),
+                    Margin = 30
+                };
+                btnYaCreado = true;
+
+                vSLButtons.Children.Add(boton3);
+            }
+            boton3.Clicked += new EventHandler(ClickBtn3); 
+        }
+
+        private void ClickBtn3(object sender, EventArgs e)
         {
-            Button  boton3 = new Button {
-                Text = "boton1",
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.Center,
-                Background = Color.FromArgb("0008ff"),
-                HeightRequest = 70,
-                WidthRequest = 200,
-                FontFamily = "Verdana",
-                FontAttributes = FontAttributes.Bold,
-                FontSize = 16,
-                BorderColor = Color.FromArgb("#f0fc03"),
-                Margin = 30
-            };
+            vSLButtons.Children.Remove(boton1);
+            boton2.Text = "Crear controles en tiempo de ejecucion mola";
         }
     }
 }
